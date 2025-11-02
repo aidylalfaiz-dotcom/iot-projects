95537aab86c4d11cd49439ce8a30f0cf1799ddb0
Larrylurex <aidylalfaiz@gmail.com>
2025-11-02 21:40:48 +0700

    Implement ultrasonic sensor utility functions

---
+++ wavino/src/ultrasonic_utils.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+#include "ultrasonic_utils.h"
+
+int TRIG_PIN;
+int ECHO_PIN;
+
+void setupUltrasonic(int trigPin, int echoPin) {
+  TRIG_PIN = trigPin;
+  ECHO_PIN = echoPin;
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+}
+
+int getDistance() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  long duration = pulseIn(ECHO_PIN, HIGH, 30000);
+  if (duration == 0) return -1; // gagal baca
+
+  int distance = duration * 0.034 / 2;
+  return distance;
+}
